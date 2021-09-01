f64f734e6b8aa9ea6a8d8b0961a233514a9fa088
danieljimenez1337 <danieljimenez1337@gmail.com>
2021-08-31 23:42:06 -0700

    add all services

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,9 +4,11 @@ import { VehicleQuery, VehicleType } from '../types/vehicle'
 const baseUrl = "http://localhost:8080/api/v1"
 
 export const api = createApi({
+  reducerPath: 'vehiclesApi',
   baseQuery: fetchBaseQuery({
     baseUrl
   }),
+  tagTypes: ['Vehicles'],
   endpoints: (build) => ({
     getVehicles: build.query<VehicleType[], VehicleQuery>({
       query: (args) => {
@@ -14,9 +16,56 @@ export const api = createApi({
         //turn object to params
         Object.entries(args).forEach(([key, value]) =>  url += `${key}=${value}&` )
         return url
-      }
-    })
+      },
+      providesTags: (result) =>
+        result
+          ? 
+            [
+              ...result.map(({ id }) => ({ type: 'Vehicles', id } as const)),
+              { type: 'Vehicles', id: 'LIST' },
+            ]
+          : 
+            [{ type: 'Vehicles', id: 'LIST' }],
+    }),
+    getVehiclesById: build.query<VehicleType[], number>({
+      query: (id) => `/vehicles/${id}`,
+      providesTags: (result, error, id) => [{ type: 'Vehicles', id }],
+    }),
+    addVehicle: build.mutation<VehicleType, VehicleType>({
+      query(body) {
+        return {
+          url: `/vehicles`,
+          method: 'POST',
+          body,
+        }
+      },
+      invalidatesTags: [{ type: 'Vehicles', id: 'LIST' }],
+    }),
+    updateVehicle: build.mutation<VehicleType, VehicleType>({
+      query(body) {
+        return {
+          url: `/vehicles`,
+          method: 'PUT',
+          body,
+        }
+      },
+      invalidatesTags: (result, error, { id }) => [{ type: 'Vehicles', id }],
+    }),
+    deletePost: build.mutation<{ success: boolean; id: number }, number>({
+      query(id) {
+        return {
+          url: `vehicles/${id}`,
+          method: 'DELETE',
+        }
+      },
+      invalidatesTags: (result, error, id) => [{ type: 'Vehicles', id }],
+    }),
   })
 })
 
-export const { useGetVehiclesQuery } = api
+export const { 
+  useGetVehiclesQuery,
+  useAddVehicleMutation,
+  useDeletePostMutation,
+  useUpdateVehicleMutation 
+} = api
